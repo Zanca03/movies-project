c882c7ce3f25c404361b7c81ccaaef1d51a67422
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2023-08-27 21:59:29 -0300

    Create the first GET Request Method

--- src/main/java/dev/carlosezpereira/movieproject/controllers/MovieController.java
+++ src/main/java/dev/carlosezpereira/movieproject/controllers/MovieController.java
@@ -1,5 +1,8 @@
 package dev.carlosezpereira.movieproject.controllers;
 
+import dev.carlosezpereira.movieproject.entities.Movie;
+import dev.carlosezpereira.movieproject.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +11,15 @@ import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> allMovies(){
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> allMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
     }
 }
