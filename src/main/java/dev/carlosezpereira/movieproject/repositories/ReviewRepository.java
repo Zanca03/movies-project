f1accaf5af7f978df6773fb48da0ca82fc0adb62
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2023-08-28 21:19:44 -0300

    Create Controller, Repository and Service for Review Entity.

---
+++ src/main/java/dev/carlosezpereira/movieproject/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.carlosezpereira.movieproject.repositories;
+
+import dev.carlosezpereira.movieproject.entities.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
