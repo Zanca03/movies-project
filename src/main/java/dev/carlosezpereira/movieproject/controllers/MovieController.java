d0e0aee2b462ccc4187274815d129a1516f3a216
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2023-08-28 21:19:04 -0300

    Make some ajusts

--- src/main/java/dev/carlosezpereira/movieproject/controllers/MovieController.java
+++ src/main/java/dev/carlosezpereira/movieproject/controllers/MovieController.java
@@ -2,24 +2,27 @@ package dev.carlosezpereira.movieproject.controllers;
 
 import dev.carlosezpereira.movieproject.entities.Movie;
 import dev.carlosezpereira.movieproject.services.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
     private MovieService movieService;
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<Movie>> allMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.getMovie(imdbId), HttpStatus.OK);
     }
 }
--- src/main/java/dev/carlosezpereira/movieproject/services/MovieService.java
+++ src/main/java/dev/carlosezpereira/movieproject/services/MovieService.java
@@ -2,10 +2,12 @@ package dev.carlosezpereira.movieproject.services;
 
 import dev.carlosezpereira.movieproject.entities.Movie;
 import dev.carlosezpereira.movieproject.repositories.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,6 +15,10 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 
 
